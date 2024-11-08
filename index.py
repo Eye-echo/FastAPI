de978d56a919330cd94c6cdd57c7c55eb8a047b3
Karimov <121497820+AGK-1@users.noreply.github.com>
2024-11-08 14:46:56 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
 grades = [67, 100, 87, 56]
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+class Pet(db.Model):
+    name = db.StringProperty(required=True)
+    type = db.StringProperty(required=True, choices=set(["cat", "dog", "bird"]))
+    birthdate = db.DateProperty()
+    weight_in_pounds = db.IntegerProperty()
+    spayed_or_neutered = db.BooleanProperty()
